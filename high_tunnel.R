698ad971f62bd95cf3a38c6d7b3058ee4ba7b073
lucasnell <lucas@lucasnell.com>
2017-08-24 14:55:07 -0400

    Working version including plotting

--- high_tunnel.R
+++ high_tunnel.R
@@ -29,149 +29,6 @@ any_complex <- function(x) {
     any(sapply(x, function(xx) !identical(Im(xx), 0)))
 }
 
-
-# HighTunnelExptSimfunct <- function(
-    # xr,xs,yr,ys,a,fresist,k,kp,kk,h,sw,s1,s2,rho,sm,Tmax,nfield,kill,
-    # harvesttimes,da,dw,pred) {
-    
-    # global totStage stageInts mumInts sj sa relatt sexratio Lr Ls clone
-
-    Su <- 0.2015^2 * diag(1, 2)
-    
-    # increased ME for mummies for development time
-    Su[2,2] <- sm * Su[2,2]
-    
-    nx  <- totStage
-    ny <- sum(mumInts) + 1
-    
-    Xr <- matrix(0, Tmax, nfield)
-    Yr <- matrix(0, Tmax, nfield)
-    Xs <- matrix(0, Tmax, nfield)
-    Ys <- matrix(0, Tmax, nfield)
-    
-    LLr <- Matrix(Lr, sparse = TRUE)
-    LLs <- Matrix(Ls, sparse = TRUE)
-    
-    # for (t in 1:Tmax) {
-    t=1
-        # for (i in 1:nfield) {
-    i=1
-            ypr <- yr[1:mumInts[1], i]
-            ypr <- ypr[ypr>0]
-            yps <- ys[1:mumInts[1], i]
-            yps <- yps[yps>0]
-            
-            sxyt <- sum(c(xr[,i], xs[,i], ypr, yps))
-            Kt <- 1 / (1 + k * sxyt)
-            Kpt <- 1 / (1 + kp * sxyt)
-            
-            
-            mm <- rbind(a * relatt[clone[1,1],] * (yr[nrow(yr),i] + ys[nrow(ys),i]) / 
-                            (h * sxyt + 1))
-            AA <- (1 + t(mm) / kk)
-            As <- AA^(-kk)
-            
-            mm <- rbind(a * relatt[clone[2,1],] * (yr[nrow(yr),i] + ys[nrow(ys),i]) / 
-                            (h * sxyt + 1))
-            AA <- (1 + t(mm) / kk)
-            Ar <- AA^(-kk) + fresist[1] * t(mm) * AA^(-kk-1) + fresist[2] *
-                (1-(AA^(-kk) + t(mm) * AA^(-kk-1)))
-            
-            xtr <- (pred * Kt * Ar) * as.matrix(LLr %*% xr[,i])
-            xts <- (pred * Kt * As) * as.matrix(LLs %*% xs[,i])
-            
-            yt <- yr[,i]
-            y <- yr[,i]
-            x <- xr[,i]
-            yt[length(yt)] <- sw * y[length(y)] + sexratio * y[(length(y)-1)]
-            yt[(mumInts[1]+2):(length(yt)-1)] <- pred * y[(mumInts[1]+1):(length(y)-2)]
-            yt[2:(mumInts[1]+1)] <- Kpt * sj[clone[1,1]] * y[1:mumInts[1]]
-            yt[1] <- (Kpt * t(matrix(1,totStage,1) - Ar)) %*% as.matrix(LLr %*% x)
-            ytr <- yt
-            
-            yt <- ys[,i]
-            y <- ys[,i]
-            x <- xs[,i]
-            yt[length(yt)] <- sw * y[length(y)] + sexratio * y[(length(y)-1)]
-            yt[(mumInts[1]+2):(length(yt)-1)] <- pred * y[(mumInts[1]+1):(length(y)-2)]
-            yt[2:(mumInts[1]+1)] <- Kpt * sj[clone[2,1]] * y[1:mumInts[1]]
-            yt[1] <- (Kpt * t(matrix(1,totStage,1) - As)) %*% as.matrix(LLs %*% x)
-            yts <- yt
-            
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # Left off on this part: He's using a matrix for the end of a sequence
-            # The behavior of this in matlab is to just end at the first element of 
-            # the matrix
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            if (t %in% harvesttimes[i,1:(ncol(harvesttimes)-1)]) {
-                xtr <- kill * xtr
-                ytr[1:mumInts] <- kill * ytr[1:mumInts]
-                ytr[(mumInts+1):(length(ytr)-1)] <- 0
-                
-                xts <- kill * xts
-                yts[1:mumInts] <- kill * yts[1:mumInts]
-                yts[(mumInts+1):(end-1)] <- 0
-            }
-            
-            xr[,i] <- xtr
-            xs[,i] <- xts
-            yr[,i] <- ytr
-            ys[,i] <- yts
-    #     }
-    # }
-    
-    dispersing <- da * t(mean(t(xr[(sum(stageInts[clone[1,1],1:4])+1):end,])))
-    xr[(sum(stageInts[clone[1,1],1:4])+1):end,] <- (1-da) * 
-        xr[(sum(stageInts[clone[1,1],1:4])+1):end,] + 
-        dispersing * matrix(1,1,nfield)
-    
-    dispersing <- da * t(mean(t(xr[(sum(stageInts[clone[2,1],1:4])+1):end,])))
-    xs[(sum(stageInts[clone[2,1],1:4])+1):end,] <- (1-da) * 
-        xs[(sum(stageInts[clone[2,1],1:4])+1):end,] + 
-        dispersing * matrix(1,1,nfield)
-    
-    dispersingw <- dw * mean(yr[end,])
-    yr[end,] <- ((1-dw) * yr[end,] + dispersingw)
-    
-    dispersingw <- dw * mean(ys[end,])
-    ys[end,] <- ((1-dw) * ys[end,] + dispersingw)
-    
-    nap <- nx + mumInts[1]
-    
-    yy <- cbind(xr, yr)
-    Xr[t,] <- sum(yy[1:nap,]);
-    Yr[t,] <- sum(yy[(nx+1):nap,]) / sum(yy[1:nap,])
-    
-    yy <- cbind(xs, ys)
-    Xs[t,] <- sum(yy[1:nap,])
-    Ys[t,] <- sum(yy[(nx+1):nap,]) / sum(yy[1:nap,])
-
-    out_list <- list(Xr = Xr, Xs = Xs, Yr = Yr, Ys = Ys, xr = xr, xs = xs, 
-                     yr = yr, ys = ys)
-    
-    
-    
-    
-#     return(out_list)
-# }
-
-
-
-
-
-
-
-
-
-
-# Here I started turning every * (not .*) to matrix multiplication %*%
-
 # global totStage stageInts mumInts sj sa relatt sexratio Lr Ls clone
 
 
@@ -218,7 +75,7 @@ repro <- rbind(
     c(0, 3.1975, 5.4563, 6.2996, 6.7372, 6.9030, 6.8210, 6.6100, 6.1962, 5.1653, 4.1837, 
       3.6029, 3.1023, 2.4799, 1.6909, 1.1750, 1.0148, 0.9096, 
       0.7821, 0.6430, 0.5000, 0.3531)
-    )
+)
 repro <- cbind(repro, matrix(0,2,178))
 
 
@@ -353,7 +210,140 @@ harvesttimes <- rbind(c(cyclelength * (1:1), cyclelength * (2:cycles)),
 
 xs <- xsinit
 xr <- xrinit
-# out_list <- HighTunnelExptSimfunct(xr,xs,yr,ys,a,fresist,k,kp,kk,h,sw,s1,s2,rho,sm,Tmax,nfield,kill,harvesttimes,da,dw,pred)
+
+
+
+
+
+
+
+
+
+
+HighTunnelExptSimfunct <- function(
+    xr,xs,yr,ys,a,fresist,k,kp,kk,h,sw,s1,s2,rho,sm,Tmax,nfield,kill,
+    harvesttimes,da,dw,pred) {
+    
+    # global totStage stageInts mumInts sj sa relatt sexratio Lr Ls clone
+
+    Su <- 0.2015^2 * diag(1, 2)
+    
+    # increased ME for mummies for development time
+    Su[2,2] <- sm * Su[2,2]
+    
+    nx  <- totStage
+    ny <- sum(mumInts) + 1
+    
+    Xr <- matrix(0, Tmax, nfield)
+    Yr <- matrix(0, Tmax, nfield)
+    Xs <- matrix(0, Tmax, nfield)
+    Ys <- matrix(0, Tmax, nfield)
+    
+    LLr <- Matrix(Lr, sparse = TRUE)
+    LLs <- Matrix(Ls, sparse = TRUE)
+    
+    for (t in 1:Tmax) {
+        for (i in 1:nfield) {
+            ypr <- yr[1:mumInts[1], i]
+            ypr <- ypr[ypr>0]
+            yps <- ys[1:mumInts[1], i]
+            yps <- yps[yps>0]
+            
+            sxyt <- sum(c(xr[,i], xs[,i], ypr, yps))
+            Kt <- 1 / (1 + k * sxyt)
+            Kpt <- 1 / (1 + kp * sxyt)
+            
+            
+            mm <- rbind(a * relatt[clone[1,1],] * (yr[nrow(yr),i] + ys[nrow(ys),i]) / 
+                            (h * sxyt + 1))
+            AA <- (1 + t(mm) / kk)
+            As <- AA^(-kk)
+            
+            mm <- rbind(a * relatt[clone[2,1],] * (yr[nrow(yr),i] + ys[nrow(ys),i]) / 
+                            (h * sxyt + 1))
+            AA <- (1 + t(mm) / kk)
+            Ar <- AA^(-kk) + fresist[1] * t(mm) * AA^(-kk-1) + fresist[2] *
+                (1-(AA^(-kk) + t(mm) * AA^(-kk-1)))
+            
+            xtr <- (pred * Kt * Ar) * as.matrix(LLr %*% xr[,i])
+            xts <- (pred * Kt * As) * as.matrix(LLs %*% xs[,i])
+            
+            yt <- cbind(yr[,i])
+            y <- cbind(yr[,i])
+            x <- cbind(xr[,i])
+            yt[length(yt)] <- sw * y[length(y)] + sexratio * y[(length(y)-1)]
+            yt[(mumInts[1]+2):(length(yt)-1)] <- pred * y[(mumInts[1]+1):(length(y)-2)]
+            yt[2:(mumInts[1]+1)] <- Kpt * sj[clone[1,1]] * y[1:mumInts[1]]
+            yt[1] <- (Kpt * t(matrix(1,totStage,1) - Ar)) %*% as.matrix(LLr %*% x)
+            ytr <- yt
+            
+            yt <- cbind(ys[,i])
+            y <- cbind(ys[,i])
+            x <- cbind(xs[,i])
+            yt[length(yt)] <- sw * y[length(y)] + sexratio * y[(length(y)-1)]
+            yt[(mumInts[1]+2):(length(yt)-1)] <- pred * y[(mumInts[1]+1):(length(y)-2)]
+            yt[2:(mumInts[1]+1)] <- Kpt * sj[clone[2,1]] * y[1:mumInts[1]]
+            yt[1] <- (Kpt * t(matrix(1,totStage,1) - As)) %*% as.matrix(LLs %*% x)
+            yts <- yt
+            
+            if (t %in% harvesttimes[i,1:(ncol(harvesttimes)-1)]) {
+                # Tony used a matrix for the end of a sequence (1:mumInts).
+                # The behavior of this in matlab is to just end at the first element of 
+                # the matrix (i.e., 1:mumInts[1,1])
+                mumInts_ <- mumInts[1,1]
+                xtr <- kill * xtr
+                ytr[1:mumInts_] <- kill * ytr[1:mumInts_]
+                ytr[(mumInts_+1):(length(ytr)-1)] <- 0
+                
+                xts <- kill * xts
+                yts[1:mumInts_] <- kill * yts[1:mumInts_]
+                yts[(mumInts_+1):(length(yts)-1)] <- 0
+            }
+            
+            xr[,i] <- xtr
+            xs[,i] <- xts
+            yr[,i] <- ytr
+            ys[,i] <- yts
+        }
+        
+        dispersing <- da * cbind(rowMeans(xr[(sum(stageInts[clone[1,1],1:4])+1):nrow(xr),]))
+        xr[(sum(stageInts[clone[1,1],1:4])+1):nrow(xr),] <- (1-da) *
+            xr[(sum(stageInts[clone[1,1],1:4])+1):nrow(xr),] + 
+            dispersing %*% matrix(1,1,nfield)
+        
+        dispersing <- da * cbind(rowMeans(xs[(sum(stageInts[clone[2,1],1:4])+1):nrow(xs),]))
+        xs[(sum(stageInts[clone[2,1],1:4])+1):nrow(xs),] <- (1-da) *
+            xs[(sum(stageInts[clone[2,1],1:4])+1):nrow(xs),] + 
+            dispersing %*% matrix(1,1,nfield)
+        
+        dispersingw <- dw * mean(yr[nrow(yr),])
+        yr[nrow(yr),] <- ((1-dw) * yr[nrow(yr),] + dispersingw)
+        
+        dispersingw <- dw * mean(ys[nrow(ys),])
+        ys[nrow(ys),] <- ((1-dw) * ys[nrow(ys),] + dispersingw)
+        
+        nap <- nx + mumInts[1]
+        
+        yy <- rbind(xr, yr)
+        Xr[t,] <- colSums(yy[1:nap,])
+        Yr[t,] <- colSums(yy[(nx+1):nap,]) / colSums(yy[1:nap,])
+        
+        yy <- rbind(xs, ys)
+        Xs[t,] <- colSums(yy[1:nap,])
+        Ys[t,] <- colSums(yy[(nx+1):nap,]) / colSums(yy[1:nap,])
+    }
+    
+
+    out_list <- list(Xr = Xr, Xs = Xs, Yr = Yr, Ys = Ys, xr = xr, xs = xs, 
+                     yr = yr, ys = ys)
+    
+    return(out_list)
+}
+
+
+
+out_list <- HighTunnelExptSimfunct(xr,xs,yr,ys,a,fresist,k,kp,kk,h,sw,s1,s2,rho,sm,
+                                   Tmax,nfield,kill,harvesttimes,da,dw,pred)
 # Assigning out_list values to global ones for objects Xr, Xs, Yr, Ys, xr, xs, yr, & ys
 invisible(
     lapply(names(out_list), 
@@ -365,6 +355,12 @@ invisible(
 Ymax <- 1.2 * max(max(Xr+Xs))
 Ymax2 <- 1.2 * max(max(Xr+Xs))
 range <- c(0, Tmax, 0, Ymax)
-
-
 Tmin <- 1
+
+# Figure 1
+plot(1:(Tmax-Tmin+1),Xr[Tmin:Tmax,1]+Xs[Tmin:Tmax,1], type = 'l', col = 'dodgerblue')
+lines(1:(Tmax-Tmin+1),Xr[Tmin:Tmax,2]+Xs[Tmin:Tmax,2], col = 'dodgerblue')
+lines(1:(Tmax-Tmin+1),Ymax2 * (Yr[Tmin:Tmax,1]+Ys[Tmin:Tmax,1]), col = 'firebrick')
+lines(1:(Tmax-Tmin+1),Ymax2 * (Yr[Tmin:Tmax,2]+Ys[Tmin:Tmax,2]), col = 'firebrick', lty = 2)
+lines(1:(Tmax-Tmin+1),Ymax2*(Xr[Tmin:Tmax,1]+Xr[Tmin:Tmax,2])/
+         (Xr[Tmin:Tmax,1]+Xr[Tmin:Tmax,2]+Xs[Tmin:Tmax,1]+Xs[Tmin:Tmax,2]), col = 'black')
