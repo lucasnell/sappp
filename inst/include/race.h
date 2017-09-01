2ec1576242d5a420f7e7070fa46e22ca0924363d
lucasnell <lucas@lucasnell.com>
2017-09-01 09:54:42 -0500

    Moved files into package setup

---
+++ inst/include/race.h
@@ -0,0 +1,12 @@
+#ifndef ACE__RACE_H
+#define ACE__RACE_H
+
+
+// This will load Rcpp.h as well
+#include <RcppArmadillo.h>
+
+using namespace std;
+using namespace Rcpp;
+using namespace arma;
+
+#endif
---
+++ src/race_types.h
@@ -0,0 +1,16 @@
+
+#include <RcppArmadillo.h>
+#include <sitmo.h>
+#include <cmath>
+#include <random>
+
+
+using namespace Rcpp;
+using namespace std;
+using namespace arma;
+
+
+//[[Rcpp::depends(RcppArmadillo, sitmo)]]
+//[[Rcpp::plugins(cpp11)]]
+
+
